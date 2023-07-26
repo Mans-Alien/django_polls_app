adfacf2829403caca2fda8c336bb8ab59e588ead
Mans-Alien <mans012033945420@gmail.com>
2023-07-27 02:56:35 +0300

    create the results.html file and put radio choices in detail.html page and post the result id and += 1 to the vote of the choosen choice

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Question
-from django.http import HttpResponse
-
+from .models import Question, Choice
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
@@ -20,9 +20,21 @@ def detail(request, question_id):
 
 
 
-
 def results(request, question_id):
-    return HttpResponse(f"you're looking at the results of  question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
 
 def vote(request, question_id):
-    return HttpResponse(f"you're voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request, "polls/detail.html", 
+            {"question": question, "error_message": "you did not select a choice."},
+        )
+    else:
+        selected_choice.vote += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
